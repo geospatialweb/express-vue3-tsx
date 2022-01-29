00aa237453d15fbb21ecf08c2f45a9f057f76284
John Campbell <johncampbell@geospatialweb.ca>
2022-01-29 14:45:55 -0500

    add IHttpCodes interface

--- node/src/interfaces/index.ts
+++ node/src/interfaces/index.ts
@@ -2,3 +2,12 @@ export interface IQueryParams {
   fields: string
   table: string
 }
+
+export interface IHttpCodes {
+  OK: number
+  CREATED: number
+  BAD_REQUEST: number
+  UNAUTHORIZED: number
+  NOT_FOUND: number
+  INTERNAL_SERVER_ERROR: number
+}
