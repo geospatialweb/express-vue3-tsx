3c6039c5495f9252aaca75c2f4dc760845146c4b
John Campbell <johncampbell@geospatialweb.ca>
2022-01-30 16:30:23 -0500

    remove ControllerService & refactor functionality into controller

--- node/src/controllers/index.ts
+++ node/src/controllers/index.ts
@@ -1,14 +1,46 @@
 import { Request, Response } from 'express'
 import { Container } from 'typedi'
 
-import { ControllerService } from '../services'
+import { HTTP_CODES } from '../enums'
+import { IHttpCodes, IQueryParams } from '../interfaces'
+import { ApiService, LogService } from '../services'
+
+const logRequest = (method: string, url: string): void => {
+  const white = '\x1b[37m%s\x1b[0m'
+  const logService: LogService = Container.get(LogService)
+  logService.consoleLog(`${method} ${url}`, white)
+}
+
+const logResponse = (status: number): void => {
+  const green = '\x1b[32m%s\x1b[0m'
+  const logService: LogService = Container.get(LogService)
+  logService.consoleLog(`JSON response status ${status}`, green)
+}
+
+const sendResponse = (res: Response, response: any): Response<any, Record<string, string>> => {
+  const { OK }: IHttpCodes = HTTP_CODES
+  logResponse(OK)
+  return res
+    .set({
+      'Access-Control-Allow-Origin': '*'
+    })
+    .status(OK)
+    .json(response)
+}
 
 export async function getGeoJsonFeatureCollection(req: Request, res: Response): Promise<Response> {
-  const controllerService: ControllerService = Container.get(ControllerService)
-  return await controllerService.getGeoJsonFeatureCollection(req, res)
+  /* prettier-ignore */
+  const { method, originalUrl, query: { fields, table } } = req
+  logRequest(method, originalUrl)
+  const apiService: ApiService = Container.get(ApiService)
+  const fc = await apiService.getGeoJsonFeatureCollection({ fields, table } as IQueryParams)
+  return sendResponse(res, fc)
 }
 
 export function getMapboxAccessToken(req: Request, res: Response): Response {
-  const controllerService: ControllerService = Container.get(ControllerService)
-  return controllerService.getMapboxAccessToken(req, res)
+  const { method, originalUrl } = req
+  logRequest(method, originalUrl)
+  const apiService: ApiService = Container.get(ApiService)
+  const token = apiService.getMapboxAccessToken()
+  return sendResponse(res, token)
 }
--- node/src/services/index.ts
+++ node/src/services/index.ts
@@ -1,5 +1,4 @@
 export { default as ApiService } from './api'
-export { default as ControllerService } from './controller'
 export { default as GeoJsonService } from './geojson'
 export { default as LogService } from './log'
 export { default as PostgresService } from './postgres'
