daf300dc51d971e1b763251dea759d9752e52f71
John Campbell <johncampbell@geospatialweb.ca>
2022-01-30 16:27:22 -0500

    remove unused https codes

--- node/src/interfaces/index.ts
+++ node/src/interfaces/index.ts
@@ -1,13 +1,8 @@
+export interface IHttpCodes {
+  OK: number
+}
+
 export interface IQueryParams {
   fields: string
   table: string
-}
-
-export interface IHttpCodes {
-  OK: number
-  CREATED: number
-  BAD_REQUEST: number
-  UNAUTHORIZED: number
-  NOT_FOUND: number
-  INTERNAL_SERVER_ERROR: number
 }
