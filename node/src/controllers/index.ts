5222ed51cf6af39abbd185267ad2196777047446
John Campbell <johncampbell@geospatialweb.ca>
2022-01-31 12:23:45 -0500

    remove const redundancies

--- node/src/controllers/index.ts
+++ node/src/controllers/index.ts
@@ -5,18 +5,17 @@ import { HTTP_CODES } from '../enums'
 import { IHttpCodes, IQueryParams } from '../interfaces'
 import { ApiService, LogService } from '../services'
 
+const apiService: ApiService = Container.get(ApiService)
+const logService: LogService = Container.get(LogService)
+
 const logRequest = (method: string, url: string): void => {
   const white = '\x1b[37m%s\x1b[0m'
-  const logService: LogService = Container.get(LogService)
   logService.consoleLog(`${method} ${url}`, white)
 }
-
 const logResponse = (status: number): void => {
   const green = '\x1b[32m%s\x1b[0m'
-  const logService: LogService = Container.get(LogService)
   logService.consoleLog(`JSON response status ${status}`, green)
 }
-
 const sendResponse = (res: Response, response: any): Response<any, Record<string, string>> => {
   const { OK }: IHttpCodes = HTTP_CODES
   logResponse(OK)
@@ -32,7 +31,6 @@ export async function getGeoJsonFeatureCollection(req: Request, res: Response): 
   /* prettier-ignore */
   const { method, originalUrl, query: { fields, table } } = req
   logRequest(method, originalUrl)
-  const apiService: ApiService = Container.get(ApiService)
   const fc = await apiService.getGeoJsonFeatureCollection({ fields, table } as IQueryParams)
   return sendResponse(res, fc)
 }
@@ -40,7 +38,6 @@ export async function getGeoJsonFeatureCollection(req: Request, res: Response): 
 export function getMapboxAccessToken(req: Request, res: Response): Response {
   const { method, originalUrl } = req
   logRequest(method, originalUrl)
-  const apiService: ApiService = Container.get(ApiService)
   const token = apiService.getMapboxAccessToken()
   return sendResponse(res, token)
 }
