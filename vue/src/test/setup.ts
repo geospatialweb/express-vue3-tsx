8528790e4da0a6172e6ac42efaa60f6f926131b0
John Campbell <johncampbell@geospatialweb.ca>
2022-04-19 14:21:55 -0400

    add Vitest setup file

---
+++ vue/src/test/setup.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata'
+
+window.matchMedia = (query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: vi.fn(),
+  removeListener: vi.fn(),
+  addEventListener: vi.fn(),
+  removeEventListener: vi.fn(),
+  dispatchEvent: vi.fn()
+})
